a47afe783ac02188b8d58e74d6958ee120561b06
Pratul Patwari <pratul.patwari@breezeware.net>
2020-04-07 16:16:35 -0400

    local changes

--- movie-catalog-service/src/main/java/dev/pratul/moviecatalogservice/controller/MovieCatalogController.java
+++ movie-catalog-service/src/main/java/dev/pratul/moviecatalogservice/controller/MovieCatalogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import dev.pratul.moviecatalogservice.service.api.UserRatingInfo;
  * rating data and movie info service.
  * */
 
+@RefreshScope
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogController {
