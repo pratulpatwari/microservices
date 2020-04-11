be9bd338909d44e36e2ec1e98081d93048d6901f
Pratul Patwari <pratul.patwari@breezeware.net>
2020-04-11 14:53:33 -0400

    added client-positions service and configured as eureka client to pull the config from cloud config server

---
+++ client-positions/src/main/java/dev/pratul/ClientPositionsApplication.java
@@ -0,0 +1,15 @@
+package dev.pratul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ClientPositionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientPositionsApplication.class, args);
+	}
+
+}
