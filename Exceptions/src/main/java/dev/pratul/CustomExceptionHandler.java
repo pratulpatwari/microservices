577b6f148acdcba7465a734b85f3e5e682b6ded9
Pratul Patwari <pratul.patwari@breezeware.net>
2020-05-24 18:30:00 -0400

    replaced application.yml with bootstrap.yml for all the microservices

--- Exceptions/src/main/java/dev/pratul/CustomExceptionHandler.java
+++ Exceptions/src/main/java/dev/pratul/CustomExceptionHandler.java
@@ -9,8 +9,6 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.ConstraintViolationException;
 
-import org.springframework.core.NestedExceptionUtils;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -46,20 +44,20 @@ public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 	}
 
 	// DB constraint failed exception
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	public ResponseEntity<ErrorMessage> constraintVoilationException(DataIntegrityViolationException ex) {
-		String message = NestedExceptionUtils.getMostSpecificCause(ex).getMessage();
-		if (message.contains("email")) {
-			message = "Email already exists";
-		} else if (message.contains("username")) {
-			message = "Username already exists";
-		} else {
-			message = "Resource not found";
-		}
-		ErrorMessage errorMessage = new ErrorMessage(ZonedDateTime.now(), HttpStatus.CONFLICT.value(),
-				Arrays.asList(message));
-		return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
-	}
+//	@ExceptionHandler(DataIntegrityViolationException.class)
+//	public ResponseEntity<ErrorMessage> constraintVoilationException(DataIntegrityViolationException ex) {
+//		String message = NestedExceptionUtils.getMostSpecificCause(ex).getMessage();
+//		if (message.contains("email")) {
+//			message = "Email already exists";
+//		} else if (message.contains("username")) {
+//			message = "Username already exists";
+//		} else {
+//			message = "Resource not found";
+//		}
+//		ErrorMessage errorMessage = new ErrorMessage(ZonedDateTime.now(), HttpStatus.CONFLICT.value(),
+//				Arrays.asList(message));
+//		return new ResponseEntity<>(errorMessage, HttpStatus.CONFLICT);
+//	}
 
 	// when the element requested does not exists
 	@ExceptionHandler(value = { NoSuchElementException.class })
