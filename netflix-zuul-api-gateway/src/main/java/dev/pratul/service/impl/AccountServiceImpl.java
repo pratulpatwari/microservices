446d9ac0c7d913a4c6c867ab0b0662d4391ed03d
Pratul Patwari <pratul.patwari@breezeware.net>
2020-06-17 22:27:55 -0400

    fixed jdbcUrl

--- netflix-zuul-api-gateway/src/main/java/dev/pratul/service/impl/AccountServiceImpl.java
+++ netflix-zuul-api-gateway/src/main/java/dev/pratul/service/impl/AccountServiceImpl.java
@@ -1,13 +1,6 @@
 package dev.pratul.service.impl;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.Set;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,6 +8,7 @@ import org.springframework.stereotype.Service;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 import dev.pratul.model.Account;
+import dev.pratul.service.api.AccountService;
 import dev.pratul.service.api.IAccountService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,42 +16,14 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class AccountServiceImpl implements IAccountService {
 
-//	@Autowired
-//	private AccountService accountService;
-
 	@Autowired
-	ServiceCallables serviceCall;
+	private AccountService accountService;
 
 	@Override
 	@HystrixCommand(fallbackMethod = "getFallbackAccountByUserId", commandKey = "zuulGateway")
 	public Set<Account> getAccountByUserId(String userId) {
 		log.info("Entering getAccountByUserId() for user {}", userId);
-		Set<Account> accounts = new HashSet<>();
-		ExecutorService executor = null;
-		try {
-			executor = Executors.newFixedThreadPool(10);
-			List<Future<Set<Account>>> list = new LinkedList<Future<Set<Account>>>();
-			List<ServiceCallables> task = new LinkedList<>();
-			for (int i = 0; i < 5; i++) {
-				task.add(serviceCall);
-			}
-			list = executor.invokeAll(task);
-			for (int i = 0; i < list.size(); i++) {
-				
-				log.info("Counter value for thread: {}", serviceCall.getCounter());
-				Future<Set<Account>> future = list.get(i);
-				try {
-					Set<Account> result = future.get();
-					accounts.addAll(result);
-				} catch (InterruptedException | ExecutionException e) {
-					e.printStackTrace();
-				}
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			executor.shutdown();
-		}
+		Set<Account> accounts = accountService.getAccountByUserId("1");
 		log.info("Leaving getAccountByUserId() for user {}", userId);
 		return accounts;
 	}
--- netflix-zuul-api-gateway/src/main/java/dev/pratul/service/impl/ServiceCallables.java
+++ netflix-zuul-api-gateway/src/main/java/dev/pratul/service/impl/ServiceCallables.java
@@ -15,17 +15,29 @@ public class ServiceCallables implements Callable<Set<Account>> {
 
 	@Autowired
 	private AccountService accountService;
-	
+
 	@Getter
 	private int counter;
-	
+
 	@Override
 	public Set<Account> call() throws Exception {
 		counter++;
 		System.out.println("Thread : " + Thread.currentThread() + "    Current value: " + counter);
 		return accountService.getAccountByUserId("1");
 	}
-	
-	
-
+	/*
+	 * private Set<Account> multiThreadForLoadTesting() { Set<Account> accounts =
+	 * new HashSet<>(); ExecutorService executor = null; try { executor =
+	 * Executors.newFixedThreadPool(10); List<Future<Set<Account>>> list = new
+	 * LinkedList<Future<Set<Account>>>(); List<ServiceCallables> task = new
+	 * LinkedList<>(); for (int i = 0; i < 5; i++) { task.add(serviceCall); } list =
+	 * executor.invokeAll(task); for (int i = 0; i < list.size(); i++) {
+	 * 
+	 * log.info("Counter value for thread: {}", serviceCall.getCounter());
+	 * Future<Set<Account>> future = list.get(i); try { Set<Account> result =
+	 * future.get(); accounts.addAll(result); } catch (InterruptedException |
+	 * ExecutionException e) { e.printStackTrace(); } } } catch
+	 * (InterruptedException e) { e.printStackTrace(); } finally {
+	 * executor.shutdown(); } return accounts; }
+	 */
 }
