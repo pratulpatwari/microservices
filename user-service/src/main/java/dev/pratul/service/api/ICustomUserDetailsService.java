7163bb9cf3e5e18cd202ce8c95225eb60f29672a
Pratul Patwari <pratul_patwari@yahoo.com>
2021-03-14 15:51:41 -0400

    updated comments on user service

--- user-service/src/main/java/dev/pratul/service/api/ICustomUserDetailsService.java
+++ user-service/src/main/java/dev/pratul/service/api/ICustomUserDetailsService.java
@@ -7,42 +7,77 @@ import dev.pratul.dto.UserDto;
 
 public interface ICustomUserDetailsService {
 
-	/*
-	 * Return the list of all available roles
+	/**
+	 * @return RoleDto array
+	 * 
+	 * @apiNote Return the list of all available roles
 	 */
 	RoleDto[] getAllRoles();
 
-	/*
-	 * Fetch the list of all users in the DB
+	/**
+	 * @return List of UserDto
+	 * 
+	 * @apiNote Return the list of all users in the DB
 	 */
 	List<UserDto> getAllUsers();
 
-	/*
-	 * Register a new user using the DTO object. Status is by default marked as
-	 * active which means any user created will be default be active
+	/**
+	 * @param userDto - user which needs to be onboarded
+	 * 
+	 * @return UserDto with userId
+	 * 
+	 * @apiNote Register a new user using the DTO object. Status is by default
+	 *          marked as active which means any user created will be default active
+	 * 
+	 * @throws IllegalArgumentException
 	 */
 	UserDto registerUser(UserDto userDto);
 
-	/*
-	 * Fetch the user using it's unique identifier
+	/**
+	 * @param userId
+	 * 
+	 * @return UserDto
+	 * 
+	 * @apiNote Return the user using it's unique identifier
+	 * 
+	 * @throws IllegalArgumentException
 	 */
 	UserDto getUserById(Long userId);
 
-	/*
-	 * Fetch the list of all users requested using query parameter. If multiple
-	 * users are to be requested, id needs to be sent with comma separated values
+	/**
+	 * @param ids comma separated
+	 * 
+	 * @return List of all users requested
+	 * 
+	 * @apiNote Return the list of all users requested using query parameter. If
+	 *          multiple users are to be requested, id needs to be sent with comma
+	 *          separated values
+	 * 
+	 * @throws IllegalArgumentException
 	 */
 	List<UserDto> getUsersByUserIds(String ids);
 
-	/*
-	 * Fetch the list of all users using their Ids
+	/**
+	 * @param ids List of user ids
+	 * 
+	 * @return List of UserDto
+	 * 
+	 * @apiNote Return the list of requested users
+	 * 
+	 * @throws IllegalArgumentException
 	 */
 	List<UserDto> getUsersByIds(List<Long> ids);
 
-	/*
-	 * This method expects every field to be present in request DTO object. If the
-	 * field needs to be same as previous value, then same value needs to be set in
-	 * the DTO
+	/**
+	 * @param userDto
+	 * 
+	 * @return UserDto with updated details
+	 * 
+	 * @apiNote This method expects every field to be present in request DTO object.
+	 *          If the field needs to be same as previous value, then same value
+	 *          needs to be set in the DTO
+	 * 
+	 * @throws IllegalArgumentException
 	 */
 	UserDto updateUserDetails(UserDto userDto);
 }
